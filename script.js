526b9e221d3aad9dff5c2ed480f8ad5983ff6304
Ion Stici <ionsticidev@gmail.com>
2022-12-30 13:31:25 +0200

    Finish Styling

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".form__input");
+const btn = document.querySelector(".form__btn");
+const message = document.querySelector(".form__message");
+
+const wrong = function () {
+    message.style.opacity = "1";
+};
+
+const check = function (e) {
+    e.preventDefault();
+};
+
+form.addEventListener("submit", check);
+btn.addEventListener("click", check);
